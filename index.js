e7c5dcd7d776362fba89b314c705bec10c7929c2
Kamran Babazadeh <kbabazadeh@icloud.com>
2024-01-25 21:54:05 +0100

    chore: Clean up code

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
+let l = function () {
+	console.log.apply(console, arguments);
+};
+
 import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
 const body = document.querySelector("body");
 
 let educationData = [];
-// // Define the width and height of the SVG canvas
-// const width = 960;
-// const height = 600;
-
-// // Create an SVG container
-// const svg = d3
-// 	.select("body") // You can select another container if needed
-// 	.append("svg")
-// 	.attr("width", width)
-// 	.attr("height", height);
-
-// // Choose a suitable projection (e.g., Albers USA)
-// const projection = d3
-// 	.geoAlbersUsa()
-// 	.translate([width / 2, height / 2])
-// 	.scale(1000);
-
-// // Create a path generator
-// const path = d3.geoPath().projection(projection);
 
 fetch(
 	"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
 )
 	.then((response) => response.json())
-	.then((educationData) => {
-		// Handle data from the first JSON file
-		console.log("Education:", educationData);
+	.then((fetchedEducationData) => {
+		// // Handle data from the first JSON file
+		educationData = fetchedEducationData.slice();
+
 		// Fetch the second JSON file
 		return fetch(
 			"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
@@ -41,35 +27,55 @@ fetch(
 		const countiesTJSON = usTJSON.objects.counties;
 		const statesTJSON = usTJSON.objects.states;
 		const nationTJSON = usTJSON.objects.nation;
-		console.log("TopoJSON:", usTJSON);
 
-		// GEOJSON DATA
-		const countiesGJSON = topojson.feature(usTJSON, countiesTJSON).features;
+		// CONVERT TO GEOJSON DATA
+		let countiesGJSON = topojson.feature(usTJSON, countiesTJSON);
 		const statesGJSON = topojson.feature(usTJSON, statesTJSON).features;
 		const nationGJSON = topojson.feature(usTJSON, nationTJSON).features[0];
-		console.log("GeoJSON counties:", countiesGJSON);
-		console.log("GeoJSON states:", statesGJSON);
-		console.log("GeoJSON nation:", nationGJSON);
+
+		// Merge ALL Education Data with County Data
+		for (const countyElement of countiesGJSON.features) {
+			//Find corresponding edu object
+			const correspondingElement = educationData.find(
+				(educationElement) => educationElement.fips == countyElement.id
+			);
+			//Merge
+			countyElement.properties = correspondingElement;
+		}
+		console.log(countiesGJSON);
+
+		for (let i = 0; i < 10; i++) {}
 
 		// This link will lead to the solution:
 		// https://observablehq.com/@d3/choropleth/2?intent=fork
 
 		//lets plot
 		const plot = Plot.plot({
-			projection: "albers-usa",
+			projection: "identity",
+			width: 975,
+			height: 610,
 			color: {
-				type: "quantile",
-				n: 9,
+				type: "quantize",
+				n: 8,
+				domain: [1, 80],
 				scheme: "blues",
 				label: "Education (%)",
 				legend: true,
 			},
 			marks: [
-				Plot.geo(counties, {
-					fill: (d) => d.properties.bachelorOrHigher,
+				Plot.geo(countiesGJSON, {
+					fill: (d) => d.properties.bachelorsOrHigher,
 					title: (d) =>
-						`${d.properties.countyName}, ${d.properties.statenNameAbgekuerzt}: ${d.properties.bachelorOrHigher}`,
+						`${d.properties.area_name}, ${d.properties.state}: ${d.properties.bachelorsOrHigher}`,
 				}),
+				Plot.geo(
+					topojson.mesh(
+						usTJSON,
+						usTJSON.objects.states,
+						(a, b) => a !== b
+					),
+					{ stroke: "white" }
+				),
 			],
 		});
 		body.append(plot);
